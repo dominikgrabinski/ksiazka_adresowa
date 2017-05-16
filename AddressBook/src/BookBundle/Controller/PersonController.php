02cce24325f73a97ce30b45565686e3828ab9767
dominikgrabinski <grabinski.dominik@gmail.com>
2017-05-16 10:04:07 +0200

    kilka dodatkow

--- AddressBook/src/BookBundle/Controller/PersonController.php
+++ AddressBook/src/BookBundle/Controller/PersonController.php
@@ -5,8 +5,12 @@ namespace BookBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 use BookBundle\Entity\Person;
+use BookBundle\Entity\Address;
+
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class PersonController extends Controller
 {
@@ -78,31 +82,96 @@ class PersonController extends Controller
     {
         $em = $this->getDoctrine()->getManager();
         $person = $em->getRepository('BookBundle:Person')->find($id);
-        $em->remove($person);
+        if($person == NULL) {
+            $resp = new Response ('Brak id: ' .$id. ' w bazie');
+        }else{
+            $em->remove($person);
+        
         $em->flush();
-        return $this->render('BookBundle:Person:delete.html.twig', array(
-            
-        ));
+        
+        $resp = new Response ('Usunięto wpis o id: '.$id);
+        }
+      return $resp;
+//        $this->render('BookBundle:Person:delete.html.twig', array(
+//            'person' => $person
+//        ));
     }
 
-    /**
-     * @Route("/{id}")
-     */
-    public function idAction($id)
-    {
-        return $this->render('BookBundle:Person:id.html.twig', array(
-            // ...
-        ));
-    }
+
 
     /**
-     * @Route("/")
+     * @Route("/showAll")
      */
     public function showAllAction()
     {
+        $repo2 = $this->getDoctrine()->getRepository('BookBundle:Person')->findAll();
+        //$count = count($repo2);
         return $this->render('BookBundle:Person:show_all.html.twig', array(
-            // ...
+            'all' => $repo2 //, 'number' => $count
         ));
     }
 
+    /**
+     * @Route("/show/{id}")
+     */
+    public function idAction($id)
+    {
+        $repo = $this->getDoctrine()->getRepository('BookBundle:Person')->find($id);
+//      //  $show = __toString($repo);
+//        $resp = new Response ('name: '.$repo->getName().'</br> surname: '.$repo->getSurname().'</br> description: '.$repo->getDescription().'</br> photo: '.$repo->getPhoto() );
+//        return $resp;
+        
+        
+        return $this->render('BookBundle:Person:id.html.twig', array(
+            'person' => $repo //, 'number' => $count
+        ));
+    }
+
+    private function getAddressForm(
+            $address,
+            $url = false,
+            $label = 'Create Address'
+            ){
+//        if($url == false){
+//            $url = $this->generateUrl('book_address_new');
+//        }
+        $form = $this->createFormBuilder($address)
+                ->setAction($url)
+                ->setMethod('POST')
+                ->add('city')
+                ->add('street')
+                ->add('home_number')
+                ->add('flat_number')
+                ->add('person', EntityType::class,[
+                    'class' => 'BookBundle\Entity\Person',
+                    'choice_label' => 'name'])
+                ->add('save', 'submit', ['label' => $label])
+                ->getForm();
+        return $form;
+                
+    }
+    
+    /**
+     * @Route("/newAddress")
+     */
+    public function newAddressAction(Request $req)
+    {
+        $newAddress = new Address();
+        $form = $this->getAddressForm($newAddress);
+        $form->handleRequest($req);
+        if($form->isSubmitted()){
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($newAddress);
+            $em->flush();
+            
+        //    return $this->redirect($this->generateUrl('book_address_showall'));
+        }
+        
+        return $this->render('BookBundle:Person:newAddress.html.twig', array(
+            'form' => $form->createView()
+        ));
+    }    
+    
+    
+    
 }
